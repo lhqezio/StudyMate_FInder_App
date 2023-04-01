fc7dc0d7d19708d380f8a4eb920598c3ddc51636
Mohamed.1 <mohamed.loutfi@dawsoncollege.qc.ca>
2023-04-01 11:31:55 -0400

    Added TestEventCalendarAddEvent in EventManagerTest

--- StudyMateTests/EventManagerTest.cs
+++ StudyMateTests/EventManagerTest.cs
@@ -18,4 +18,31 @@ public class EventCalendarTest
         Assert.AreEqual(lEvents, eM);
         Assert.IsInstanceOfType(eM, typeof(EventManager));
     }
+
+    [TestMethod]
+    public void TestEventCalendarAddEvent(){//Test 2 - Check if event is added to List
+        //Arrange
+        EventManager eM = new EventManager();
+        
+        List<Courses> nCourses1 = new List<Courses>(Art, Calculus, Math, History);
+        List<Courses> nCourses2 = new List<Courses>(Business, Communication, Journalism, Political_Science);
+        List<Courses> nCourses3 = new List<Courses>(Sciences, Statistics, Chemistry, Linear_Algebra);
+        Profile profil1 = new Profile{"Alain", 25, "Hb", nCourses1};
+        Profile profil2 = new Profile{"Sam", 20, "St-Ex", nCourses2};
+        Profile profil3 = new Profile{"Jack", 15, "PST", nCourses3};
+        List<Profile> profileList = new List<Profile>(profil2, profil3);
+        DateTimeOffset dTime = new DateTimeOffset();
+        bool sent = false;
+        string description = "SDASDASDK DAS DDASKD DSAD ASASDDAS DA";
+        List<Courses> eventCourses = new List<Courses>(Sciences, Art, Business, Math);
+        List<string> subjectSchoolProjectList = new List<string>("Informatique, Vaudreuil, StudyMate");
+
+        //Act
+        eM.AddEvent(eC);
+        EventCalendar eC = new EventCalendar("Title1", profil1, profileList, dTime, sent, description, eventCourses, SubjectSchoolProjectList);
+
+        //Assert
+        Assert.AreEqual(eC, eM.listEvents[0]);
+    }
+
 }
