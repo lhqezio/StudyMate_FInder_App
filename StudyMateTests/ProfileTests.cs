3c559cf79ff458c697ac07aa8a28b4585d645469
Seyed Amirreza Mojtahedi <seyed.mojtahedi@dawsoncollege.qc.ca>
2023-04-04 11:06:40 -0400

    Added test methods for the Profile Class that checks if two objects of this type are Equal and if the ClearProfile actually works.

--- StudyMateTests/ProfileTests.cs
+++ StudyMateTests/ProfileTests.cs
@@ -15,4 +15,28 @@ public class ProfileTests
         // Assert
         Assert.AreEqual(profile1,profile2);
     }
+
+    [TestMethod]
+    public void EqualsTest_ComparesTwoProfileObjects_ReturnsTrue()
+    {
+        //Arrange
+        Profile profile1=new Profile("Amir",Genders.Male,20,"Dawson College",new List<Courses>(){Courses.History});
+        Profile profile2=new Profile("Amir",Genders.Male,20,"Dawson College",new List<Courses>(){Courses.History});
+        // //Act
+        bool AreEqual=profile1.Equals(profile2);
+        // Assert
+        Assert.AreEqual(true,AreEqual);
+    }
+
+    [TestMethod]
+    public void EqualsTest_ComparesTwoProfileObjects_ReturnsFalse()
+    {
+        //Arrange
+        Profile profile1=new Profile("Amir",Genders.Male,20,"Dawson College",new List<Courses>(){Courses.History});
+        Profile profile2=new Profile("Samanta",Genders.Female,18,"Vanier",new List<Courses>(){Courses.Computer_Science});
+        // //Act
+        bool AreNotEqual=profile1.Equals(profile2);
+        // Assert
+        Assert.AreEqual(false,AreNotEqual);
+    }
 }
