056ee58bde1796cbf681a7d4227d44c10c74eba5
Seyed Amirreza Mojtahedi <seyed.mojtahedi@dawsoncollege.qc.ca>
2023-04-11 10:30:29 -0400

    Added env variables

--- src/DBcontext/StudyMateDbContext.cs
+++ src/DBcontext/StudyMateDbContext.cs
@@ -7,17 +7,13 @@ namespace StudyMate
 {
     public class StudyMateDbContext : DbContext
     {
-        public DbSet<Profile> Profiles { get; set; }
-        public DbSet<User> Users { get; set; }
+        public DbSet<Profile>? Profiles { get; set; }
+        public DbSet<User>? Users { get; set; }
         // The following configures EF to connect to an oracle database
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
-            Console.Write("Enter the username for the database: ");
-            string username = Console.ReadLine();
-
-            Console.Write("Enter the password for the database: ");
-            string password = Console.ReadLine();
-
-            optionsBuilder.UseOracle(@$"User Id={username}; Password={password};
+            string? oracleUser=Environment.GetEnvironmentVariable("ORACLE_APP_USER");
+            string? oraclePassword=Environment.GetEnvironmentVariable("ORACLE_APP_PASSWORD");
+            optionsBuilder.UseOracle(@$"User Id={oracleUser}; Password={oraclePassword};
 		        Data Source=198.168.52.211:1521/pdbora19c.dawsoncollege.qc.ca;");
         }
     }
