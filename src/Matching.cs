392680045fbdc497db43f4fc369cd2831ebcd44c
Seyed Amirreza Mojtahedi <seyed.mojtahedi@dawsoncollege.qc.ca>
2023-04-14 11:24:01 -0400

    Almost finished the BestMatches method using a dictionary to know the best matches best on the points

--- src/Matching.cs
+++ src/Matching.cs
@@ -11,13 +11,25 @@ namespace StudyMate
         public List<Profile> BestMatches(){
             using (var dbContext = new StudyMateDbContext())
             {
+                Dictionary<int, int> bestMatches = new Dictionary<int, int>();
                 List<Profile> profiles = dbContext.Profiles.ToList();
                 for (int i = 0; i < profiles.Count; i++)
                 {
+                    int points=0;
                     if(profiles[i].Equals(this.__profile)){
                         continue;
                     }
-                    
+                    for (int x = 0; x < __profile.NeedHelpCourses.Count; x++)
+                    {
+                        for (int y = 0; y < profiles[i].CanHelpCourses.Count; y++)
+                        {
+                            if (__profile.NeedHelpCourses[x]==profiles[i].CanHelpCourses[y])
+                            {
+                                points++;
+                            }
+                        }
+                    }
+                    bestMatches.Add(points,i);
                 }
                 return profiles;
             }
