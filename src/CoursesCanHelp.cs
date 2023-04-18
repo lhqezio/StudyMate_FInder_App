9b56c8ee9dec4e8ed0b6cb3bef20b99703be3be4
Seyed Amirreza Mojtahedi <seyed.mojtahedi@dawsoncollege.qc.ca>
2023-04-18 09:02:37 -0400

    Added a new class called CoursesCanHelp that links Profile with courses that a profile can help with

---
+++ src/CoursesCanHelp.cs
@@ -0,0 +1,18 @@
+namespace StudyMate
+{
+    public class CoursesCanHelp
+    {
+        public int CourseId { get; set; }
+        public Courses Course { get; set; }
+        public string ProfileId { get; set; }
+        public Profile profile{get;set;}=null!;        
+        public CoursesCanHelp()
+        {
+        }
+
+        public CoursesCanHelp(Courses course)
+        {
+            Course = course;
+        }
+    }
+}
