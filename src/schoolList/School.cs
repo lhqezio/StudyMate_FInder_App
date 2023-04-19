45c9533da01f12f7f5809e68f843975d8ff7228e
Mohamed.1 <mohamed.loutfi@dawsoncollege.qc.ca>
2023-04-19 14:27:47 -0400

    Separated school.cs into 2 files (school.cs and schoolManager.cs to separate purpose)

--- src/schoolList/School.cs
+++ src/schoolList/School.cs
@@ -1,8 +1,3 @@
-using System.Linq;
-using System.Collections.Generic;
-using System.Globalization;
-using System.IO;
-using CsvHelper;
 
 
 namespace Study
@@ -13,18 +8,6 @@ namespace Study
 
         public School(string name){
             Name = name;
-        }
-
-        public List<School> LoadSchoolFile(string file){
-            List<School> schools = new List<School>();
-            var lines = File.ReadAllLines(file).Skip(1);
-            foreach (var line in lines)
-            {
-                string[] fields = line.Split(',');
-                School school = new School(fields[0]);
-                schools.Add(school);
-            }
-            return schools;
         }
     }
 
