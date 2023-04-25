71df22ea609234fcbd0f0b458c205e21607cd205
Seyed Amirreza Mojtahedi <seyed.mojtahedi@dawsoncollege.qc.ca>
2023-04-25 15:56:19 -0400

    Fixed tests for Profile class

--- StudyMateTest/ProfileTests.cs
+++ StudyMateTest/ProfileTests.cs
@@ -18,42 +18,44 @@ public class ProfileTests
         Assert.AreEqual(profile1,profile2);
     }
 
-    // [TestMethod]
-    // public void EqualsTest_ComparesTwoProfileObjects_ReturnsTrue()
-    // {
-    //     //Arrange
-    //     UserDB userDB=new UserDB("amirreza","amir@example.com","ABCD","pwd");
-    //     Profile profile1=new Profile("Amir",20,"Dawson College",new List<NeedHelpCourses>(){new NeedHelpCourses(Courses.History)},userDB);
-    //     Profile profile2=new Profile("Amir",20,"Dawson College",new List<NeedHelpCourses>(){new NeedHelpCourses(Courses.History)},userDB);
-    //     //Act
-    //     bool AreEqual=profile1.Equals(profile2);
-    //     //Assert
-    //     Assert.AreEqual(true,AreEqual);
-    // }
+    [TestMethod]
+    public void EqualsTest_ComparesTwoProfileObjects_ReturnsTrue()
+    {
+        //Arrange
+        User user1=new User("amirreza","PK1","1");
+        User user2=new User("Samanta","PK2","2");
+        Profile profile1=new Profile("Amir",20,new School("Dawson College"),"Computer Science",new List<NeedHelpCourses>(){new NeedHelpCourses(Courses.History)},user1,Genders.Male);
+        Profile profile2=new Profile("Amir",20,new School("Dawson College"),"Computer Science",new List<NeedHelpCourses>(){new NeedHelpCourses(Courses.History)},user1,Genders.Male);
+        //Act
+        bool AreEqual=profile1.Equals(profile2);
+        //Assert
+        Assert.AreEqual(true,AreEqual);
+    }
 
-    // [TestMethod]
-    // public void EqualsTest_ComparesTwoProfileObjects_ReturnsFalse()
-    // {
-    //     //Arrange
-    //     UserDB userDB=new UserDB("amirreza","amir@example.com","ABCD","pwd");
-    //     Profile profile1=new Profile("Amir",20,"Dawson College",new List<NeedHelpCourses>(){new NeedHelpCourses(Courses.History)},userDB);
-    //     Profile profile2=new Profile("Samanta",18,"Vanier",new List<NeedHelpCourses>(){new NeedHelpCourses(Courses.Computer_Science)},userDB);
-    //     //Act
-    //     bool AreNotEqual=profile1.Equals(profile2);
-    //     //Assert
-    //     Assert.AreEqual(false,AreNotEqual);
-    // }
+    [TestMethod]
+    public void EqualsTest_ComparesTwoProfileObjects_ReturnsFalse()
+    {
+        //Arrange
+        User user1=new User("amirreza","PK1","1");
+        User user2=new User("Samanta","PK2","2");
+        Profile profile1=new Profile("Amir",20,new School("Dawson College"),"Computer Science",new List<NeedHelpCourses>(){new NeedHelpCourses(Courses.History)},user1,Genders.Male);
+        Profile profile2=new Profile("Samanta",18,new School("Vanier College"),"Social Science",new List<NeedHelpCourses>(){new NeedHelpCourses(Courses.Math)},user2,Genders.Female);
+        //Act
+        bool AreNotEqual=profile1.Equals(profile2);
+        //Assert
+        Assert.AreEqual(false,AreNotEqual);
+    }
 
-    // [TestMethod]
-    // public void EqualsTest_ComparesProfileObjectWithSomethingElse_ReturnsFalse()
-    // {
-    //     //Arrange
-    //     UserDB userDB=new UserDB("amirreza","amir@example.com","ABCD","pwd");
-    //     Profile profile1=new Profile("Amir",20,"Dawson College",new List<NeedHelpCourses>(){new NeedHelpCourses(Courses.History)},userDB);
-    //     String s=new String("Hi");
-    //     //Act
-    //     bool AreNotEqual=profile1.Equals(s);
-    //     //Assert
-    //     Assert.AreEqual(false,AreNotEqual);
-    // }
+    [TestMethod]
+    public void EqualsTest_ComparesProfileObjectWithSomethingElse_ReturnsFalse()
+    {
+        //Arrange
+        User user1=new User("amirreza","PK1","1");
+        Profile profile1=new Profile("Amir",20,new School("Dawson College"),"Computer Science",new List<NeedHelpCourses>(){new NeedHelpCourses(Courses.History)},user1,Genders.Male);
+        String s=new String("Hi");
+        //Act
+        bool AreNotEqual=profile1.Equals(s);
+        //Assert
+        Assert.AreEqual(false,AreNotEqual);
+    }
 }
