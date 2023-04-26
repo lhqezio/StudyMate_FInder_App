5347cd8414335346f99584ab843209fec48194c6
Mohamed.1 <mohamed.loutfi@dawsoncollege.qc.ca>
2023-04-26 11:08:09 -0400

    fixed addparticipants test

--- StudyMateTest/EventCalendarTest.cs
+++ StudyMateTest/EventCalendarTest.cs
@@ -102,7 +102,7 @@ public class EventCalendarTest
             //userLis list
         List<User> userList = new List<User>();
         userList.Add(user2);
-        userList.Add(user3)
+        userList.Add(user3);
         DateTimeOffset dTime = DateTimeOffset.Now.AddMonths(1);
         bool sent = false;
         string description = "Study with the homies";
@@ -118,48 +118,15 @@ public class EventCalendarTest
         string location = "Montreal";
 
         //Act
+            //Contains user3
         EventCalendar eC = new EventCalendar("Title1", user1, userList, dTime, description, schoolList, eventCourses, location, sent);
-        
-        //Arrange
-        School sch1 = new School("Dawson College");
-        School sch2 = new School("Henri-Bourassa");
-        School sch3 = new School("Saint-Ex");
-            //Users
-        UserDB user1 = new UserDB("Alain", "alain@hotmail.com", "password");
-        UserDB user2 = new UserDB("Sam", "sam@hotmail.com", "password1");
-        UserDB user3 = new UserDB("Jack", "jack@hotmail.com", "password2");
-            //Profile
-        Profile profile1 = new Profile("Alain", 15, sch1, new List<NeedHelpCourses>(){new NeedHelpCourses(Courses.History)}, user1);
-        Profile profile2 = new Profile("Sam", 20, sch2, new List<NeedHelpCourses>(){new NeedHelpCourses(Courses.Art)}, user2);
-        Profile profile3 = new Profile("Jack", 18, sch3, new List<NeedHelpCourses>(){new NeedHelpCourses(Courses.Calculus)}, user3); 
-            //Profile list
-        List<Profile> profileList = new List<Profile>();
-        profileList.Add(profile2);
-        profileList.Add(profile3);
-        DateTimeOffset dTime = DateTimeOffset.Now.AddMonths(1);
-        bool sent = false;
-        string description = "Study with the homies";
-            //Courses
-        List<CourseEvent> eventCourses = new List<CourseEvent>();
-        CourseEvent ce1 = new CourseEvent(Courses.Math);
-        CourseEvent ce2 = new CourseEvent(Courses.Sciences);
-        CourseEvent ce3 = new CourseEvent(Courses.Business);
-        eventCourses.Add(ce1);
-        eventCourses.Add(ce2);
-        eventCourses.Add(ce3);
-        School schoolList = sch1;
-        string location = "Montreal";
-
-        //Act
-            //Contains Profile3
-        EventCalendar eC = new EventCalendar("Title1", profile1, profileList, dTime, description, schoolList, eventCourses, location, sent);
-            //Removed Profile3 to Event
-        eC.RemoveParticipant(profile3); 
-            //Removed Profile3 to profileList
-        profileList.Remove(profile3);
+            //Removed user3 to Event
+        eC.RemoveParticipant(user3); 
+            //Removed user3 to userList
+        userList.Remove(user3);
 
         //Assert
-        Assert.AreEqual(profileList, eC.Participants);
+        Assert.AreEqual(userList, eC.Participants);
     }
 
     // [TestMethod]
