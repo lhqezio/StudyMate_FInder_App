da52f3b5edb045086f807adf13be1b1d520b25ec
Seyed Amirreza Mojtahedi <seyed.mojtahedi@dawsoncollege.qc.ca>
2023-04-26 10:39:36 -0400

    Working on test for updating a profile

--- StudyMateTest/ProfileServicesTests.cs
+++ StudyMateTest/ProfileServicesTests.cs
@@ -30,13 +30,29 @@ public class ProfileServicesTests
         var profileToDelete = new Profile("Amir", 20, new School("Dawson College"), "Computer Science", new List<NeedHelpCourses>() { new NeedHelpCourses(Courses.History) }, user1, Genders.Male);
         var mockSet = new Mock<DbSet<Profile>>();
         var mockContext = new Mock<StudyMateDbContext>();
-        // mockSet.Setup(p => p.Remove(It.IsAny<Profile>())).Verifiable();
         mockContext.Setup(p => p.Profiles).Returns(mockSet.Object);
         var service = ProfileServices.getInstance(mockContext.Object);
         // Act
         service.DeleteProfile(profileToDelete, user1);
         // Assert
         mockSet.Verify(p => p.Remove(It.IsAny<Profile>()), Times.Once());
+        mockContext.Verify(p => p.SaveChanges(), Times.Once());
+    }
+
+    [TestMethod]
+    public void UpdateProfileTest_UpdatesProfileInDB_ReturnsVoid(){
+        //Arrange
+        User user1 = new User("amirreza", "PK1", "1");
+        Profile profileToUpdate = new Profile("Amir", 20, new School("Dawson College"), "Computer Science", new List<NeedHelpCourses>() { new NeedHelpCourses(Courses.History) }, user1, Genders.Male);
+        Profile updateProfile = new Profile("Amirreza", 27, new School("Concordia University"), "Art", new List<NeedHelpCourses>() { new NeedHelpCourses(Courses.Journalism), new NeedHelpCourses(Courses.Art) }, user1, Genders.Male);
+        var mockSet = new Mock<DbSet<Profile>>();
+        var mockContext = new Mock<StudyMateDbContext>();
+        mockContext.Setup(p => p.Profiles).Returns(mockSet.Object);
+        var service = ProfileServices.getInstance(mockContext.Object);
+        //Act
+        service.UpdateProfile(profileToUpdate, updateProfile, user1);
+        //Assert
+        mockSet.Verify(p => p.Attach(profileToUpdate), Times.Once());
         mockContext.Verify(p => p.SaveChanges(), Times.Once());
     }
 
