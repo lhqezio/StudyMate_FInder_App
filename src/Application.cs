2a7d44386196251243c8d61cd7130667da0bb4d4
Seyed Amirreza Mojtahedi <seyed.mojtahedi@dawsoncollege.qc.ca>
2023-04-27 08:19:37 -0400

    Working on application class

--- src/Application.cs
+++ src/Application.cs
@@ -18,19 +18,18 @@
             Profile profile1 = new Profile("alain", 18, sch,"Computer science", new List<NeedHelpCourses>(){new NeedHelpCourses(Courses.History)}, currentUser);
             
             // 3.	Create an event for user1
-            School sch2 = new School("Henri-Bourassa");
-            School sch3 = new School("Saint-Ex");
-    
+            
             //Users
-            User creator = new User("Sam", "sam@hotmail.com", "password1");
-            User participant = new User("Jack", "jack@hotmail.com", "password2");
+            User participant1 = new User("Sam", "sam@hotmail.com", "password1");
+            User participant2 = new User("Jack", "jack@hotmail.com", "password2");
             //Profiles
-            Profile creator_profile = new Profile("Sam", 20, sch2,"Education", new List<NeedHelpCourses>(){new NeedHelpCourses(Courses.Art)}, creator);
-            Profile participant_profile = new Profile("Jack", 18, sch3,"Sports studies", new List<NeedHelpCourses>(){new NeedHelpCourses(Courses.Calculus)}, participant);
+            Profile participant1_profile = new Profile("Sam", 20, new School("Henri-Bourassa"),"Education", new List<NeedHelpCourses>(){new NeedHelpCourses(Courses.Art)}, participant1);
+            Profile participant2_profile = new Profile("Jack", 18, new School("Saint-Ex"),"Sports studies", new List<NeedHelpCourses>(){new NeedHelpCourses(Courses.Calculus)}, participant2);
             //Profile list
             List<Profile> profileList = new List<Profile>();
             profileList.Add(profile1);
-            profileList.Add(participant_profile);
+            profileList.Add(participant1_profile);
+            profileList.Add(participant2_profile);
             DateTimeOffset dTime = DateTimeOffset.Now.AddMonths(1);
             bool sent = true;
             string description = "Study with the homies";
@@ -46,59 +45,59 @@
             string location = "Montreal";
 
             //Act
-            EventCalendar eC = new EventCalendar("Study event", creator_profile, profileList, dTime, description, school, eventCourses, location, sent);
+            EventCalendar eC = new EventCalendar("Study event", profile1, profileList, dTime, description, school, eventCourses, location, sent);
             profile1.Events.Add(eC);
 
             // 4.	Log out from user1
             db.Logout(currentUser.__session_key);
 
 
-            // 5.	Create a new user account (user4)
-            User user4 = User.Register("Samir", "Samir@hotmail.com", "testPassword2", db);
-            currentUser = user4;
-            UserDB userDb4 = db.Users.FirstOrDefault(u => u.Username == user4.Username);
+            // 5.	Create a new user account (user2)
+            var user2 = userService.Register("user2", "user2@hotmail.com", "testpassword");
+            currentUser = user2;
+            UserDB userDb2 = db.Users.FirstOrDefault(u => u.Username == user2.Username);
 
-            // 6.	Create a profile for user4
-            School sch4 = new School("Vanier");
-            Profile profile4 = new Profile("Samir", 18, sch4, new List<NeedHelpCourses>(){new NeedHelpCourses(Courses.Art)}, userDb4);
+            // 6.	Create a profile for user2
+            School sch2 = new School("Vanier");
+            Profile profile2 = new Profile("Samir", 18, sch2,"Health science", new List<NeedHelpCourses>(){new NeedHelpCourses(Courses.Art)}, user2);
 
             // 7.	Perform a search to find the event created by user1
             Search search = new Search(db);
             EventCalendar ec = search.SearchEventsCreator(profile1)[0];
 
-            // 8.	Mark user4 as attending user1’s event
-            ec.AddParticipant(profile4);
+            // 8.	Mark user2 as attending user1’s event
+            ec.AddParticipant(profile2);
 
-            9.	Attempt to edit user1’s event as user2 (should fail)
+            // 9.	Attempt to edit user1’s event as user2 (should fail)
             
+            
+            // 10.	Perform a search that finds user1’s profile
 
-            10.	Perform a search that finds user1’s profile
+            // 11.	Send 3 messages from user2 to user1
 
-            11.	Send 3 messages from user2 to user1
+            // 12.	Log out from user2
 
-            12.	Log out from user2
+            // 13.	Log in as user1
 
-            13.	Log in as user1
+            // 14.	Change user1’s password
 
-            14.	Change user1’s password
+            // 15.	Modify user1’s profile
 
-            15.	Modify user1’s profile
+            // 16.	Access messages, viewing text of the messages sent by user2.
 
-            16.	Access messages, viewing text of the messages sent by user2.
+            // 17.	Send a message to user2 from user1.
 
-            17.	Send a message to user2 from user1.
+            // 18.	Find and view the attendees of user1’s event
 
-            18.	Find and view the attendees of user1’s event
+            // 19.	Modify user1’s event.
 
-            19.	Modify user1’s event.
+            // 20.	Delete user1’s profile
 
-            20.	Delete user1’s profile
+            // 21.	Delete user1’s account
 
-            21.	Delete user1’s account
+            // 22.	Log in as user2
 
-            22.	Log in as user2
-
-            23.	Delete user2’s account
+            // 23.	Delete user2’s account
 
         }
     }
