2ad6a64539b63d0c0263f3b0492015b1da96f809
lhqezio <lhqezio1010@gmail.com>
2023-05-08 12:33:28 -0400

    Add basic Avalonia files

---
+++ StudyMate_GUI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace StudyMate.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ StudyMate_GUI/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace StudyMate.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
